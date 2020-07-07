35b7f90afdfaaa9a0dd23fbe72a6824fc638b9af
Sigbjørn Bore <sigbjobo@saga.sigma2.no>
2020-07-07 09:23:56 +0200

    removed import CONF *, and replaced by dictionary approach with all constants and input variables stored as CONF[var].

--- main.py
+++ main.py
@@ -1,34 +1,36 @@
 import numpy as np
 from mpi4py import MPI
 from scipy.spatial.transform import Rotation as R
-from CONF import *     # "myapp" case
-import CONF as sim 
+import sys
 import pmesh.pm as pmesh # Particle mesh Routine
+
+CONF = {}
+exec(open(sys.argv[1]).read(), CONF)
 
 # Same seed for all simulations
 np.random.seed(0)
 
 #Initialization of simulation variables
 kb=2.479/298
-if('T0' in globals()):
-    kbT0=kb*T0
-    E_kin0 = kb*3*Np*T0/2.
+if 'T0' in CONF:
+    CONF['kbT0']=kb*CONF['T0']
+    E_kin0 = kb*3*CONF['Np']*CONF['T0']/2.
 
-if 'T_start' in globals():
-    kbT_start=kb*T_start
+if 'T_start' in CONF:
+    CONF['kbT_start']=kb*CONF['T_start']
 
-Ncells = (Nv**3)
-L      = np.array(L)
-V      = L[0]*L[1]*L[2]
-dV     = V/(Nv**3)
+Ncells = (CONF['Nv']**3)
+CONF['L']      = np.array(CONF['L'])
+CONF['V']      = CONF['L'][0]*CONF['L'][1]*CONF['L'][2]
+CONF['dV']     = CONF['V']/(CONF['Nv']**3)
 
-if 'phi0' not in globals():
-    phi0=Np/V
+if 'phi0' not in CONF:
+    CONF['phi0']=CONF['Np']/CONF['V']
     
 
 #Initialize particles
-r=np.zeros((Np,3))
-vel=np.zeros((Np,3))
+r=np.zeros((CONF['Np'],3))
+vel=np.zeros((CONF['Np'],3))
 
 #Intialize MPI
 comm = MPI.COMM_WORLD
@@ -39,29 +41,29 @@ np.savetxt('start.dat',np.hstack((r,vel)))
 
 
 # Particle force array
-f=np.zeros((Np,3))
+f=np.zeros((CONF['Np'],3))
 f_old=np.copy(f)
-if "test_quasi" in globals(): 
+if "test_quasi" in CONF: 
     f_test=np.copy(f)
     f_zero=np.copy(f)
     r_zero=np.copy(r)
 
 #Particle-Mesh initialization
-pm   = pmesh.ParticleMesh((Nv,Nv,Nv),BoxSize=L, dtype='f8',comm=comm)
+pm   = pmesh.ParticleMesh((CONF['Nv'],CONF['Nv'],CONF['Nv']),BoxSize=CONF['L'], dtype='f8',comm=comm)
 density = pm.create(mode='real')
 indicies=[]
      
-if 'chi' in globals() and 'NB' in globals():
-    indicies.append(np.arange(0,Np-NB))#A
-    indicies.append(np.arange(Np-NB,Np))#B
+if 'chi' in CONF and 'NB' in CONF:
+    indicies.append(np.arange(0,CONF['Np']-CONF['NB']))#A
+    indicies.append(np.arange(CONF['Np']-CONF['NB'],CONF['Np']))#B
 
     types = 2
     layout  = [pm.decompose(r[indicies[0]]),pm.decompose(r[indicies[1]])]  
-    names=['A']*(Np-NB)+['B']*NB
+    names=['A']*(CONF['Np']-CONF['NB'])+['B']*CONF['NB']
     
 else:
-    names=['A']*Np
-    indicies.append(np.arange(Np))
+    names=['A']*CONF['Np']
+    indicies.append(np.arange(CONF['Np']))
     types=1
     layout  = [pm.decompose(r[indicies[0]])]
 
@@ -74,7 +76,7 @@ phi_t=[]
 force_ds=[]
 v_pot=[]
 v_pot_fft=[]
-if 'test_quasi' in globals():
+if 'test_quasi' in CONF:
     force_test=[]
 for t in range(types):
     phi.append(pm.create('real'))
@@ -85,7 +87,7 @@ for t in range(types):
     v_pot_fft.append(pm.create('complex'))
 
     force_ds.append([pm.create('real') for d in range(3)])
-    if 'test_quasi' in globals():
+    if 'test_quasi' in CONF:
         force_test.append([pm.create('real') for d in range(3)])
 
 # Output files
@@ -96,62 +98,62 @@ fp_E   = open('E.dat','w')
 
 def GEN_START_VEL(vel):
     #NORMAL DISTRIBUTED PARTICLES FIRST FRAME
-    std  = np.sqrt(kbT_start/mass)
-    vel = np.random.normal(loc=0, scale=std,size=(Np,3))
+    std  = np.sqrt(CONF['kbT_start']/CONF['mass'])
+    vel = np.random.normal(loc=0, scale=std,size=(CONF['Np'],3))
     
     vel = vel-np.mean(vel, axis=0)
-    fac= np.sqrt((3*Np*kbT_start/2.)/(0.5*mass*np.sum(vel**2)))
+    fac= np.sqrt((3*CONF['Np']*CONF['kbT_start']/2.)/(0.5*CONF['mass']*np.sum(vel**2)))
     
     return fac*vel
 
  
 def GEN_START_UNIFORM(r):
      
-    n=int(np.ceil(Np**(1./3.)))
-    l=L[0]/n
+    n=int(np.ceil(CONF['Np']**(1./3.)))
+    l=CONF['L'][0]/n
     x=np.linspace(0.5*l,(n-0.5)*l,n)
     
     j=0
     for ix in range(n):
         for iy in range(n):
             for iz in range(n):
-                if(j<Np):
+                if(j<CONF['Np']):
                     r[j,0]=x[ix]
                     r[j,1]=x[iy]
                     r[j,2]=x[iz]
                 
                 j+=1
-    if(j<Np):
-        r[j:]=L*np.random.random((Np-j,3))
+    if(j<CONF['Np']):
+        r[j:]=CONF['L']*np.random.random((CONF['Np']-j,3))
     return r
 
 def potential_transfer_function(k, v):
-    return (v * np.exp(-0.5*sigma**2*k.normp(p=2))**2)
+    return (v * np.exp(-0.5*CONF['sigma']**2*k.normp(p=2))**2)
 def phi_transfer_function(k, v):
-    return v * np.exp(-0.5*sigma**2*k.normp(p=2))
+    return v * np.exp(-0.5*CONF['sigma']**2*k.normp(p=2))
 
 def INTEGERATE_POS(x, vel, a):
 # Velocity Verlet integration I
-    return x + vel*dt + 0.5*a*dt**2
+    return x + vel*CONF['dt'] + 0.5*a*CONF['dt']**2
 
 def INTEGRATE_VEL(vel, a, a_old):
 # Velocity Verlet integration II
-    return vel + 0.5*(a+a_old)*dt
+    return vel + 0.5*(a+a_old)*CONF['dt']
 
 def VEL_RESCALE(vel, tau):
     #https://doi.org/10.1063/1.2408420
 
     # INITIAL KINETIC ENERGY
-    E_kin  = 0.5*mass*np.sum(vel**2)
+    E_kin  = 0.5*CONF['mass']*np.sum(vel**2)
 
     #BERENDSEN LIKE TERM
-    d1 = (E_kin0-E_kin)*dt/tau
+    d1 = (E_kin0-E_kin)*CONF['dt']/tau
 
     # WIENER NOISE
-    dW = np.sqrt(dt)*np.random.normal()
+    dW = np.sqrt(CONF['dt'])*np.random.normal()
 
     # STOCHASTIC TERM
-    d2 = 2*np.sqrt(E_kin*E_kin0/(3*Np))*dW/np.sqrt(tau)
+    d2 = 2*np.sqrt(E_kin*E_kin0/(3*CONF['Np']))*dW/np.sqrt(tau)
 
     # TARGET KINETIC ENERGY
     E_kin_target = E_kin + d1 + d2
@@ -164,14 +166,14 @@ def VEL_RESCALE(vel, tau):
     return vel
 
 def sphere(r):
-    radius=np.linalg.norm(r-L[None,:]*0.5,axis=1)
+    radius=np.linalg.norm(r-CONF['L'][None,:]*0.5,axis=1)
     ind=np.argsort(radius)[::-1]
     r=r.copy()[ind,:]
     
     return r
 
 def cube(r):
-    radius=np.max(np.abs(r-L[None,:]*0.5),axis=1)
+    radius=np.max(np.abs(r-CONF['L'][None,:]*0.5),axis=1)
     ind=np.argsort(radius)[::-1]
     r=r.copy()[ind,:]
     
@@ -179,11 +181,11 @@ def cube(r):
 
 
 def WRITE_TRJ_GRO(fp, x, vel,t):    
-    fp.write('MD of %d mols, t=%.3f\n'%(Np,t))
-    fp.write('%-10d\n'%(Np))
+    fp.write('MD of %d mols, t=%.3f\n'%(CONF['Np'],t))
+    fp.write('%-10d\n'%(CONF['Np']))
     for i in range(len(x)):
         fp.write("%5d%-5s%5s%5d%8.3f%8.3f%8.3f%8.4f%8.4f%8.4f\n"%(i//10+1,names[i],names[i],i+1,x[i,0],x[i,1],x[i,2],vel[i,0],vel[i,1],vel[i,2]))
-    fp.write("%-5.5f\t%5.5f\t%5.5f\n"%(L[0],L[1],L[2]))
+    fp.write("%-5.5f\t%5.5f\t%5.5f\n"%(CONF['L'][0],CONF['L'][1],CONF['L'][2]))
     fp.flush()
     return fp
 
@@ -211,17 +213,17 @@ def UPDATE_FIELD(comp_v_pot=False):
         # Distribute paticles
         phi[k].value[:]=0.0
         phi[k]=pm.paint(r[indicies[k]], layout=layout[k])
-        phi[k] = phi[k]/dV
+        phi[k] = phi[k]/CONF['dV']
 
         phi_fft[k] = phi[k].r2c(out=Ellipsis)
         phi_fft_tt[k]=phi_fft[k].copy().apply(potential_transfer_function, out=Ellipsis)
 
         
     if types==2:
-        v_pot_fft[0]=((1/(kappa*phi0)*(phi_fft_tt[0]+phi_fft_tt[1]) + chi/phi0*phi_fft_tt[1]))
-        v_pot_fft[1]=((1/(kappa*phi0)*(phi_fft_tt[0]+phi_fft_tt[1]) + chi/phi0*phi_fft_tt[0]))
+        v_pot_fft[0]=((1/(CONF['kappa']*CONF['phi0'])*(phi_fft_tt[0]+phi_fft_tt[1]) + CONF['chi']/CONF['phi0']*phi_fft_tt[1]))
+        v_pot_fft[1]=((1/(CONF['kappa']*CONF['phi0'])*(phi_fft_tt[0]+phi_fft_tt[1]) + CONF['chi']/CONF['phi0']*phi_fft_tt[0]))
     else:
-        v_pot_fft[0]=(1/(kappa*phi0)*phi_fft_tt[0])
+        v_pot_fft[0]=(1/(CONF['kappa']*CONF['phi0'])*phi_fft_tt[0])
         
     for t in range(types):
         force_d=[]
@@ -230,14 +232,14 @@ def UPDATE_FIELD(comp_v_pot=False):
                 return -k[d] * 1j * v 
             force_ds[t][d]=(v_pot_fft[t].apply(force_transfer_function).c2r(out=Ellipsis))
         if(comp_v_pot):
-            v_pot[t]=v_pot_fft[t].c2r(out=Ellipsis)-1./kappa
+            v_pot[t]=v_pot_fft[t].c2r(out=Ellipsis)-1./CONF['kappa']
             phi_t[t]=phi_fft[t].c2r(out=Ellipsis)
 
 def COMPUTE_ENERGY():
     E_hpf=0
     for i in range(types):
         E_hpf = pm.comm.allreduce(0.5*np.sum(v_pot[0].readout(r[indicies[i]],layout=layout[i]))) 
-    E_kin = pm.comm.allreduce(0.5*mass*np.sum(vel**2))
+    E_kin = pm.comm.allreduce(0.5*CONF['mass']*np.sum(vel**2))
  
     
     
@@ -245,15 +247,15 @@ def COMPUTE_ENERGY():
         phi_t[k]=phi[k].r2c(out=Ellipsis).apply(phi_transfer_function, out=Ellipsis).c2r(out=Ellipsis)
         
     if types==2:
-        W=dV/phi0*np.sum(0.5/kappa*(phi_t[0] + phi_t[1]-phi0)**2 + chi*phi_t[0]*phi_t[1])
+        W=CONF['dV']/CONF['phi0']*np.sum(0.5/CONF['kappa']*(phi_t[0] + phi_t[1]-CONF['phi0'])**2 + CONF['chi']*phi_t[0]*phi_t[1])
     else:
-        W = 0.5*dV/phi0*np.sum(1./kappa*(phi_t[0]-phi0)**2)
+        W = 0.5*CONF['dV']/CONF['phi0']*np.sum(1./CONF['kappa']*(phi_t[0]-CONF['phi0'])**2)
     return E_hpf,E_kin,W
     
 def ANDERSEN(vel):
     
-    std  = np.sqrt(kbT0/mass) 
-    change = np.random.random((len(vel))) < tau*dt
+    std  = np.sqrt(CONF['kbT0']/CONF['mass']) 
+    change = np.random.random((len(vel))) < taut*CONF['dt']
 
     if(sum(change)>0):
         vel[change,0]=np.random.normal(loc=0, scale=std, size=sum(change))
@@ -263,20 +265,20 @@ def ANDERSEN(vel):
     return vel
 
 def PRESET_VEL(vel,T):
-    std  = np.sqrt(kbT0/mass)
-    vel[:,0]=np.random.normal(loc=0, scale=std, size=Np)
-    vel[change,1]=np.random.normal(loc=0, scale=std, size=Np)
-    vel[change,2]=np.random.normal(loc=0, scale=std, size=Np)
+    std  = np.sqrt(CONF['kbT0']/CONF['mass'])
+    vel[:,0]=np.random.normal(loc=0, scale=std, size=CONF['Np'])
+    vel[change,1]=np.random.normal(loc=0, scale=std, size=CONF['Np'])
+    vel[change,2]=np.random.normal(loc=0, scale=std, size=CONF['Np'])
     return vel
 
 def REMOVE_CM_VEL(vel,T=None):
     if T==None:
-        E_KIN_1 = 0.5*mass*np.sum(vel**2)
+        E_KIN_1 = 0.5*CONF['mass']*np.sum(vel**2)
     else:
-        E_KIN_1 = kb*3*Np*T/2.
+        E_KIN_1 = kb*3*CONF['Np']*T/2.
  
     vcm     = -np.mean(vel,axis=0)
-    E_KIN_2 = 0.5*mass*np.sum((vel-vcm)**2)
+    E_KIN_2 = 0.5*CONF['mass']*np.sum((vel-vcm)**2)
 
     return (vel-vcm)*np.sqrt(E_KIN_1/E_KIN_2)
 
@@ -295,71 +297,71 @@ def TEST_QUASI(step,v_test,force_test):
     f_test_std=np.sqrt(np.mean(np.sum((f-f_test)**2,axis=1)))
     f_zero_std=np.sqrt(np.mean(np.sum((f-f_zero)**2,axis=1)))
                   
-    fp_test.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(step*dt,E_hpf,E_hpf_test,E_hpf_zero,f_mag, f_test_std,f_zero_std))            
+    fp_test.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(step*CONF['dt'],E_hpf,E_hpf_test,E_hpf_zero,f_mag, f_test_std,f_zero_std))            
 
 #Degrees of freedom
-if('read_start' in globals()):
+if('read_start' in CONF):
     dat = np.loadtxt(read_start)
     r   = dat[:,:3]
     vel = dat[:,3:6]
 
 else:
-    if 'uniform_start' in globals():
+    if 'uniform_start' in CONF:
         if uniform_start==True:
             r = GEN_START_UNIFORM(r)
         else:
-            r   = np.random.random((Np,3))*L
+            r   = np.random.random((CONF['Np'],3))*CONF['L']
     else:
-        r   = np.random.random((Np,3))*L
+        r   = np.random.random((CONF['Np'],3))*CONF['L']
         
 
-if 'T_start' in globals():
-    vel = np.zeros((Np,3))
+if 'T_start' in CONF:
+    vel = np.zeros((CONF['Np'],3))
     vel = GEN_START_VEL(vel) 
 
-if 'gen_sphere' in globals():
-    if gen_sphere:
+if 'gen_sphere' in CONF:
+    if CONF['gen_sphere']:
         r=sphere(r)
 
-if 'gen_cube' in globals():
-    if gen_cube:
+if 'gen_cube' in CONF:
+    if CONF[gen_cube]:
         r=cube(r)
     
-if 'rm_cm_vel' in globals():
-    if rm_cm_vel:
+if 'rm_cm_vel' in CONF:
+    if CONF['rm_cm_vel']:
         vel=REMOVE_CM_VEL(vel)    
 # For first step
 UPDATE_FIELD(True)
 COMP_FORCE(f,r,force_ds)
 
         
-if "test_quasi" in globals():    
+if "test_quasi" in CONF:    
     force_tests=force_ds.copy()
     v_test=v_pot.copy()
     r_0=r.copy()
 
-for step in range(NSTEPS):
+for step in range(CONF['NSTEPS']):
 
      
-    if(np.mod(step,nprint)==0):      
+    if(np.mod(step,CONF['nprint'])==0):      
         E_hpf, E_kin,W = COMPUTE_ENERGY()        
-        T     =   2*E_kin/(kb*3*Np)
+        T     =   2*E_kin/(kb*3*CONF['Np'])
         mom=np.sum(vel,axis=0)
-        if "test_quasi" in globals():        
+        if "test_quasi" in CONF:        
             TEST_QUASI(step,v_test,force_tests)
 
     f_old = np.copy(f)
 
     #Integrate positions
-    r     = INTEGERATE_POS(r, vel, f/mass)
+    r     = INTEGERATE_POS(r, vel, f/CONF['mass'])
 
     #PERIODIC BC
-    r     = np.mod(r, L[None,:])
+    r     = np.mod(r, CONF['L'][None,:])
 
-    if(np.mod(step+1,quasi)==0):
-        UPDATE_FIELD(np.mod(step+1,quasi)==0)
+    if(np.mod(step+1,CONF['quasi'])==0):
+        UPDATE_FIELD(np.mod(step+1,CONF['quasi'])==0)
          
-    if "test_quasi" in globals():
+    if "test_quasi" in CONF:
         if(np.mod(step+1,test_quasi)==0):
             force_tests=force_ds.copy()
             v_test=v_pot.copy()
@@ -368,17 +370,17 @@ for step in range(NSTEPS):
     COMP_FORCE(f,r,force_ds)
     
     # Integrate velocity
-    vel = INTEGRATE_VEL(vel, f/mass, f_old/mass)
+    vel = INTEGRATE_VEL(vel, f/CONF['mass'], f_old/CONF['mass'])
 
     # Thermostat
-    if('T0' in globals()):
+    if('T0' in CONF):
         #vel = ANDERSEN(vel)
         vel = VEL_RESCALE(vel,tau)
     # Print trajectory
-    if(np.mod(step,nprint)==0):
+    if(np.mod(step,CONF['nprint'])==0):
         
-        fp_trj=WRITE_TRJ_GRO(fp_trj, r, vel,dt*step)
-        fp_E.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(step*dt,W+E_kin,W,E_kin,T,mom[0],mom[1],mom[2]))
+        fp_trj=WRITE_TRJ_GRO(fp_trj, r, vel,CONF['dt']*step)
+        fp_E.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(step*CONF['dt'],W+E_kin,W,E_kin,T,mom[0],mom[1],mom[2]))
 
         fp_E.flush()
 
@@ -386,12 +388,12 @@ for step in range(NSTEPS):
 UPDATE_FIELD(True)
                               
 E_hpf, E_kin, W = COMPUTE_ENERGY()
-T     =  2*E_kin/(kb*3*Np)
+T     =  2*E_kin/(kb*3*CONF['Np'])
 mom=np.sum(vel,axis=0)
 
-fp_E.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(NSTEPS*dt,W+E_kin,W,E_kin,T,mom[0],mom[1],mom[2]))
+fp_E.write("%f\t%f\t%f\t%f\t%f\t%f\t%f\t%f\n"%(CONF['NSTEPS']*CONF['dt'],W+E_kin,W,E_kin,T,mom[0],mom[1],mom[2]))
  
 # Write last frame
-fp_trj=WRITE_TRJ_GRO(fp_trj, r, vel,dt*NSTEPS)
+fp_trj=WRITE_TRJ_GRO(fp_trj, r, vel,CONF['dt']*CONF['NSTEPS'])
 
 np.savetxt('final.dat',np.hstack((r,vel,f_old)))
