434337a08e6c1920b8280b196a9eca52b377489d
Sigbjørn Bore <sigbjobo@saga.sigma2.no>
2020-07-07 09:25:50 +0200

    Corrected for new input file convention.

--- ex.sh
+++ ex.sh
@@ -1,6 +1,5 @@
 #Example shell script of how to run the code
-cp CONF_MONO.py CONF.py 
-python3 main.py
 
-# cp CONF_BINARY.py CONF.py
-# python3 main.py
+python3 main.py CONF_MONO.py
+
+#python3 main.py CONF_BINARY.py
