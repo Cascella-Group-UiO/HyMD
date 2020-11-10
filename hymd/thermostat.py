4b5c5c7b73739df3b9e78c3fb0ac85d191027219
Morten Ledum <morten.ledum@gmail.com>
2020-11-10 19:20:04 +0100

    Add CSRV effective energy H tilde calculation
    And output it to terminal with verbose > 1 flag provided to main.py.

--- hymd/thermostat.py
+++ hymd/thermostat.py
@@ -2,17 +2,6 @@ import numpy as np
 
 
 def velocity_rescale(velocity, config, comm):
-    """ Velocity rescale thermostat, see
-        https://doi.org/10.1063/1.2408420
-            Parameters
-            ----------
-            vel : [N_mpi,3] float array beloning to MPI-task
-            tau : float, relaxation time of thermostat.
-            Returns:
-            ----------
-            out : vel
-            Thermostatted velocity array.
-    """
     K = comm.allreduce(0.5 * config.mass * np.sum(velocity**2))
     K_target = ((3 / 2) * (2.479 / 298.0)
                 * config.n_particles * config.target_temperature)
@@ -35,9 +24,12 @@ def velocity_rescale(velocity, config, comm):
     R1 = comm.bcast(R1, root=0)
     Ri2_sum = comm.bcast(Ri2_sum, root=0)
 
-    alpha2 = (+ exp1
-              + K_target / (K * N_f) * (1 - exp1) * (R1*R1 + Ri2_sum)
-              + 2 * exp2 * R1 * np.sqrt((1 - exp1) * K_target / (K * N_f)))
+    alpha2 = (
+        + exp1
+        + K_target / (K * N_f) * (1 - exp1) * (R1*R1 + Ri2_sum)
+        + 2 * exp2 * R1 * np.sqrt((1 - exp1) * K_target / (K * N_f))
+    )
     alpha = np.sqrt(alpha2)
     velocity = velocity * alpha
+    config.thermostat_work += K * (alpha2 - 1.0)
     return velocity
