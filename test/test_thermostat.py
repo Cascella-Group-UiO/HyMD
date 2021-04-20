8d7f7293cfec6b1a4c8bc2d46607093f8134a63b
Morten Ledum <morten.ledum@gmail.com>
2021-04-20 19:38:58 +0200

    Complete rewrite of test_thermostat
    Add testing of the thermostat_work with and without coupling groups.
    Streamline the random number hook into thermostat for testing with
    deterministic input/output by utilizing the new _random_gaussian and
    _random_chi_squared functions in thermostat.py.

--- test/test_thermostat.py
+++ test/test_thermostat.py
@@ -4,20 +4,44 @@ import warnings
 import pmesh
 from mpi4py import MPI
 from input_parser import Config, _find_unique_names
-from thermostat import velocity_rescale
+from thermostat import csvr_thermostat
 from field import domain_decomposition
 from file_io import distribute_input
 
 
 @pytest.mark.mpi()
 def test_thermostat_coupling_groups(molecules_with_solvent):
-    indices, positions, molecules, velocities, bonds, names, types = molecules_with_solvent
-    n_particles = len(indices)
+
+    class RandomMock:
+        ind = 0
+
+        def __init__(self, x):
+            self.x = x
+
+        def __call__(self, *args):
+            self.ind += 1
+            return self.x[self.ind - 1]
+
+    def T_from_K(K, N):
+        return 2 * K / (3 * (2.479 / 298.0) * N)
+
+    def K_from_V(V, comm=MPI.COMM_WORLD, m=72.0):
+        return 0.5 * m * comm.allreduce(np.sum(V**2), MPI.SUM)
+
+    def K_from_T(T, N):
+        return (2.479 / 298.0) * 3 * T * N / 2
+
+    (indices_global, positions_global, molecules_global, velocities_global,
+     bonds_global, names_global, types_global) = molecules_with_solvent
+
+    n_particles = len(indices_global)
     box_size = np.array([10, 10, 10], dtype=np.float64)
-    config = Config(n_steps=0, time_step=0.1, box_size=box_size, tau=0.1,
-                    mesh_size=[5, 5, 5], sigma=0.5, kappa=0.05,
-                    n_particles=len(indices), target_temperature=310.0,
-                    thermostat_work=0.0, mass=72.0)
+    config = Config(
+        n_steps=0, time_step=0.032958582578275, box_size=box_size,
+        tau=0.925852989520023, mesh_size=[2, 2, 2], sigma=0.5, kappa=0.05,
+        n_particles=len(indices_global), target_temperature=310.0,
+        thermostat_work=0.0, mass=72.0
+    )
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
@@ -27,84 +51,70 @@ def test_thermostat_coupling_groups(molecules_with_solvent):
             message=r'Creating an ndarray from ragged nested sequences'
         )
         pm = pmesh.ParticleMesh(config.mesh_size, BoxSize=config.box_size,
-                                dtype='f4', comm=comm)
+                                dtype='f8', comm=comm)
 
     # Test stub acting like a hdf5 file for distribute_input
-    in_file = {'molecules': molecules, 'indices': indices}
+    in_file = {'molecules': molecules_global, 'indices': indices_global}
     rank_range, molecules_flag = distribute_input(
-        in_file, rank, size,  config.n_particles, 6, comm=comm
+        in_file, rank, size,  config.n_particles, max_molecule_size=6,
+        comm=comm,
     )
-    positions_ = positions[rank_range]
-    molecules_ = molecules[rank_range]
-    indices_ = indices[rank_range]
-    velocities_ = velocities[rank_range]
-    bonds_ = bonds[rank_range]
-    types_ = types[rank_range]
-    names_ = names[rank_range]
+    positions = positions_global[rank_range]
+    molecules = molecules_global[rank_range]
+    indices = indices_global[rank_range]
+    velocities = velocities_global[rank_range]
+    bonds = bonds_global[rank_range]
+    types = types_global[rank_range]
+    names = names_global[rank_range]
 
     dd = domain_decomposition(
-        positions_,
-        pm,
-        indices_,
-        velocities_,
-        names_,
-        types_,
-        molecules=molecules_,
-        bonds=bonds_,
-        verbose=2,
-        comm=comm
+        positions, pm, indices, velocities, names, types, molecules=molecules,
+        bonds=bonds, verbose=2, comm=comm,
     )
-    positions_, indices_, velocities_, names_, types_, bonds_, molecules_ = dd
+    positions, indices, velocities, names, types, bonds, molecules = dd
 
-    kinetic_energy_A = 55.699870
-    kinetic_energy_B = 59.560380
-    kinetic_energy_C = 7.4953280
-    kinetic_energy_D = 45.699978
+    # Regression testing to ensure the underlying system didn't change.
+    kinetic_energy = 168.45555165866017
+    temperature = 300.0000262607726
+    kinetic_energy_species = [55.69986940118755, 59.56037533981311,
+                              7.495327677224677, 45.699979240434836]
+    temperature_species = [318.8413354377512, 318.2106019678958,
+                           600.6743707981589, 244.15927235264365]
 
-    temperature_A = 318.8413449517521
-    temperature_B = 318.2106216743212
-    temperature_C = 600.6743926201775
-    temperature_D = 244.1592650562905
+    total_kinetic_energy = K_from_V(velocities, comm=comm)
+    total_temperature = T_from_K(total_kinetic_energy, n_particles)
+    assert total_kinetic_energy == pytest.approx(kinetic_energy, abs=1e-12)
+    assert total_temperature == pytest.approx(temperature, abs=1e-12)
 
-    for t, T, K in zip(
-        ("A", "B", "C", "D"),
-        (temperature_A, temperature_B, temperature_C, temperature_D),
-        (kinetic_energy_A, kinetic_energy_B, kinetic_energy_C, kinetic_energy_D)
-    ):
-        n_particles_ = comm.allreduce(len(np.where(names_ == np.string_(t))[0]),
-                                      MPI.SUM)
-        kinetic_energy = comm.allreduce(
-            0.5 * 72.0 * np.sum(velocities_[names_ == np.string_(t)]**2),
-            MPI.SUM
+    print("TOTAL NPARTICLES, KINETIC, TEMP", n_particles,
+          total_kinetic_energy, total_temperature)
+
+    for t, T, K in zip(("A", "B", "C", "D"), temperature_species,
+                       kinetic_energy_species):
+        n_particles_species = comm.allreduce(
+            len(np.where(names == np.string_(t))[0]), MPI.SUM
         )
-        temperature = (2.0 / 3.0) * kinetic_energy / ((2.479 / 298.0) * n_particles_)
-        assert K == pytest.approx(kinetic_energy, abs=1e-3)
-        assert T == pytest.approx(temperature, abs=1e-3)
+        K_species = K_from_V(velocities[names == np.string_(t)], comm=comm)
+        T_species = T_from_K(K_species, n_particles_species)
+        assert K == pytest.approx(K_species, abs=1e-13)
+        assert T == pytest.approx(T_species, abs=1e-13)
+        print("SPECIES, NPARTICLES, KINETIC, TEMP", t, n_particles_species,
+              K_species, T_species)
 
     config = _find_unique_names(config, names, comm=comm)
-    velocities__ = velocities_.copy()
-    velocities__ = velocity_rescale(velocities__, names_, config, comm,
-                                    R1=0.005, Ri2_sum=134.0026)
+    velocities_copy = velocities.copy()
 
-    kinetic_energy = comm.allreduce(
-        0.5 * config.mass * np.sum(velocities__**2),
-        MPI.SUM
+    # Hijack the _random_gaussian and _random_chi_squared interal functions in
+    # thermostat to control the exact numbers generated for testing.
+    random_chi_squared = RandomMock([125.4595634810623])
+    random_gaussian = RandomMock([0.5579657512081987])
+    velocities_copy = csvr_thermostat(
+        velocities_copy, names, config, comm=comm,
+        random_gaussian=random_gaussian, random_chi_squared=random_chi_squared,
     )
-    temperature = (2.0 / 3.0) * kinetic_energy / ((2.479 / 298.0) * n_particles)
-    assert temperature == pytest.approx(305.0, abs=1e-2)
-
-    for t in ("A", "B", "C", "D"):
-        n_particles_ = comm.allreduce(len(np.where(names_ == np.string_(t))[0]),
-                                      MPI.SUM)
-        kinetic_energy = comm.allreduce(
-            0.5 * 72.0 * np.sum(velocities__[names_ == np.string_(t)]**2),
-            MPI.SUM
-        )
-        temperature = (2.0 / 3.0) * kinetic_energy / ((2.479 / 298.0) * n_particles_)
-        assert np.abs(temperature - 305.0) > 1.0
-
-    thermostat_work = (3.0 / 2.0) * ((2.479 / 298.0) * n_particles) * (305.0 - 300.0)
-    assert config.thermostat_work == pytest.approx(thermostat_work, abs=1e-2)
+    kinetic_energy_new = K_from_V(velocities_copy, comm=comm)
+    assert kinetic_energy_new == pytest.approx(171.257138006274, abs=1e-13)
+    assert config.thermostat_work == pytest.approx(2.80158634761387, abs=1e-13)
 
     config.thermostat_work = 0.0
     config.thermostat_coupling_groups = [
@@ -113,35 +123,51 @@ def test_thermostat_coupling_groups(molecules_with_solvent):
         ["C"],
         ["D"],
     ]
-    velocities__ = velocities_.copy()
+    velocities_copy = velocities.copy()
+    random_chi_squared = RandomMock([35.43824087713971, 27.57022975113815,
+                                     2.024725328228174, 36.50208472031436])
+    random_gaussian = RandomMock([-1.752320325907187, 1.099694957420625,
+                                  0.6113448515745533, -0.7183266831611322])
+    velocities_copy = csvr_thermostat(
+        velocities_copy, names, config, comm=comm,
+        random_gaussian=random_gaussian, random_chi_squared=random_chi_squared,
+    )
 
-    R1 = [
-        0.10845486735451927,
-        -0.059371471410613480,
-        0.016628906557081095,
-        0.285001319212115300,
+    K_species = np.array([
+        K_from_V(velocities_copy[names == np.string_("A")], comm=comm),
+        K_from_V(velocities_copy[names == np.string_("B")], comm=comm),
+        K_from_V(velocities_copy[names == np.string_("C")], comm=comm),
+        K_from_V(velocities_copy[names == np.string_("D")], comm=comm),
+    ])
+    K_expected = np.array([50.03215278458857, 62.31604075323946,
+                           8.039648643032598, 43.74504593736311])
+
+    assert np.allclose(K_species, K_expected, atol=1e-13, rtol=0.0)
+    assert config.thermostat_work == pytest.approx(-4.322663540436441,
+                                                   abs=1e-13)
+
+    config.thermostat_work = 0.0
+    config.thermostat_coupling_groups = [
+        ["A", "B", "C"],
+        ["D"],
     ]
-    Ri2_sum = [
-        39.144906813432560,
-        43.789014283383494,
-        1.2255582018575327,
-        46.754797887862255,
-    ]
-    velocities__ = velocity_rescale(velocities__, names_, config, comm,
-                                    R1=R1, Ri2_sum=Ri2_sum)
-    for t in ("A", "B", "C", "D"):
-        n_particles_ = comm.allreduce(len(np.where(names_ == np.string_(t))[0]),
-                                      MPI.SUM)
-        kinetic_energy = comm.allreduce(
-            0.5 * 72.0 * np.sum(velocities__[names_ == np.string_(t)]**2),
-            MPI.SUM
+    velocities_copy = velocities.copy()
+    random_chi_squared = RandomMock([97.07130218590895, 33.06359496718739])
+    random_gaussian = RandomMock([0.1661408606772054, -0.06216797747541603])
+    velocities_copy = csvr_thermostat(
+        velocities_copy, names, config, comm=comm,
+        random_gaussian=random_gaussian, random_chi_squared=random_chi_squared,
+    )
+
+    inds_ABC = np.where(
+        np.logical_or.reduce(
+            (names == np.string_("A"), names == np.string_("B"),
+             names == np.string_("C"))
         )
-        temperature = (2.0 / 3.0) * kinetic_energy / ((2.479 / 298.0) * n_particles_)
-        assert temperature == pytest.approx(305.0, abs=1e-1)
-
-    kinetic_energy = comm.allreduce(0.5 * 72.0 * np.sum(velocities__**2), MPI.SUM)
-    temperature = temperature = (2.0 / 3.0) * kinetic_energy / ((2.479 / 298.0) * n_particles)
-    assert temperature == pytest.approx(305.0, abs=1e-1)
-
-    thermostat_work = (3.0 / 2.0) * ((2.479 / 298.0) * n_particles) * (305.0 - 300.0)
-    assert config.thermostat_work == pytest.approx(thermostat_work, abs=1e-1)
+    )
+    K_group_ABC = K_from_V(velocities_copy[inds_ABC], comm=comm)
+    K_group_D = K_from_V(velocities_copy[names == np.string_("D")], comm=comm)
+    assert K_group_ABC == pytest.approx(123.6090634948376, abs=1e-13)
+    assert K_group_D == pytest.approx(45.41754237593906, abs=1e-13)
+    assert config.thermostat_work == pytest.approx(0.5710542121164457,
+                                                   abs=1e-13)
