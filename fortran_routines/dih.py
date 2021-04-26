30d9c1271e39193ba7f9e3aca47838b21e1f36cd
Manuel Carrer <manuelca@kjemi.uio.no>
2021-04-26 18:20:38 +0200

    Implement plain dihedral forces

--- fortran_routines/dih.py
+++ fortran_routines/dih.py
@@ -2,64 +2,76 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ##
-a = np.array([50, 30, 10], dtype=np.float64)
-b = np.array([60, 32, 35], dtype=np.float64)
-c = np.array([65, 44, 23], dtype=np.float64)
-d = np.array([69, 50, 44], dtype=np.float64)
-
-##
-def norm(v):
-    s = v[0] ** 2 + v[1] ** 2 + v[2] ** 2
-    return np.sqrt(s)
-
-
-def get_dihedral(a, b, c, d):
-    b1 = b - a
-    b2 = c - b
-    b3 = d - c
-
-    b1 /= norm(b1)
-    b2 /= norm(b2)
-    b3 /= norm(b3)
-
-    # Cross products
-    n1 = np.cross(b1, b2)
-    n2 = np.cross(b2, b3)
-    m = np.cross(n1, b2)
-
-    # Dot products
-    x = np.dot(n1, n2)
-    y = np.dot(m, n2)
-
-    return np.arctan2(y, x)
-
-
-def new_dihedral(a, b, c, d):
-    # Trans dihedral = 0
+def new_dihedral(p1, p2, p3, p4):
+    # Angle calculation, taken from:
+    # https://stackoverflow.com/a/34245697
+    # Trans dihedral = 180
     # Positive dihedral moving clockwise
-    b0 = -(b - a)
-    b1 = -(c - b)
-    b2 = d - c
+    b0 = p1 - p2
+    b1 = p2 - p3
+    b2 = p4 - p3
+    bn = np.linalg.norm(b1)
 
-    bv = b1 / np.linalg.norm(b1)
+    bv = b1 / bn
     v = b0 - np.dot(b0, bv) * bv
     w = b2 - np.dot(b2, bv) * bv
 
-    x = np.dot(v, w)
-    y = np.dot(np.cross(bv, v), w)
-
-    fig = plt.figure(facecolor="white", figsize=(10, 10))
-    ax = plt.axes(projection="3d")
-    for i in (a, b, c, d):
-        ax.scatter(i[0], i[1], i[2], s=100)
-    ax.quiver(*b, *b0, arrow_length_ratio=0.05)
-    ax.quiver(*c, *b1, arrow_length_ratio=0.05)
-    ax.quiver(*c, *b2, arrow_length_ratio=0.05)
-
-    ax.quiver(*b, *v, arrow_length_ratio=0.05, color="r")
-    ax.quiver(*b, *w, arrow_length_ratio=0.05, color="g")
-
+    x = np.dot(v, w)  # cos(phi)
+    y = np.dot(np.cross(bv, v), w)  # sin(phi)
     return np.arctan2(y, x)
+
+
+def dV_dphi(p1, p2, p3, p4, ci, di, phi):
+    # Force calculation, taken from:
+    # A. Blondel, M. Karplus, JCC Vol 17, No 9, 1132-1141 (1996)
+    b0, b1, b2, bn = prepare_vectors(p1, p2, p3, p4)
+
+    # Cross products
+    a = np.cross(b0, b1)
+    b = np.cross(b2, b1)
+    # Squared norms
+    a2 = np.dot(a, a)
+    b2 = np.dot(b, b)
+    # Dot products
+    fg = np.dot(b0, b1)
+    hg = np.dot(b2, b1)
+    s = a * fg / (a2 * bn) - b * hg / (b2 * bn)
+    # Forces on particles
+    df = 0
+    for i in range(len(ci)):
+        df += i * ci[i] * np.sin(i * phi + di[i])
+
+    F1 = df * -bn * a / a2
+    F4 = df * bn * b / b2
+    F2 = df * s - F1
+    F3 = df * -s - F4
+    return F1, F2, F3, F4
+
+
+def prepare_vectors(p1, p2, p3, p4):
+    b0 = p1 - p2
+    b1 = p2 - p3
+    b2 = p4 - p3
+
+    for i, b in enumerate(box):
+        b0[i] -= b * np.rint(b * b0[i])
+        b1[i] -= b * np.rint(b * b1[i])
+        b2[i] -= b * np.rint(b * b2[i])
+
+    bn = np.linalg.norm(b1)
+    return b0, b1, b2, bn
+
+
+#     fig = plt.figure(facecolor="white", figsize=(10, 10))
+#     ax = plt.axes(projection="3d")
+#     for i in (a, b, c, d):
+#         ax.scatter(i[0], i[1], i[2], s=100)
+#     ax.quiver(*b, *b0, arrow_length_ratio=0.05)
+#     ax.quiver(*c, *b1, arrow_length_ratio=0.05)
+#     ax.quiver(*c, *b2, arrow_length_ratio=0.05)
+#
+#     ax.quiver(*b, *v, arrow_length_ratio=0.05, color="r")
+#     ax.quiver(*b, *w, arrow_length_ratio=0.05, color="g")
 
 
 def plot_dih(a, b, c, d):
@@ -77,10 +89,15 @@ def plot_dih(a, b, c, d):
 
 
 ##
-a = np.array([0, 0, 0], dtype=np.float64)
-b = np.array([np.sqrt(2) / 2, np.sqrt(2) / 2, 0], dtype=np.float64)
-c = np.array([np.sqrt(2) / 2 + 1, np.sqrt(2) / 2, 0], dtype=np.float64)
-d = np.array([np.sqrt(2) / 2 + 2, np.sqrt(2), -0.02], dtype=np.float64)
-print(np.degrees(get_dihedral(a, b, c, d)))
-print(np.degrees(new_dihedral(a, b, c, d)))
+a = np.array([-1, 0, 0], dtype=np.float64)
+b = np.array([0, 1, 0], dtype=np.float64)
+c = np.array([2, 1, 0], dtype=np.float64)
+d = np.array([3, 2, 0], dtype=np.float64)
+# print(np.degrees(get_dihedral(a, b, c, d)))
+# print(np.degrees(new_dihedral(a, b, c, d)))
 # plot_dih(a, b, c, d)
+##
+a = np.array([10, 10, 10], dtype=np.float64)
+print(np.dot(a, a))
+print(np.linalg.norm(a))
+print(np.linalg.norm(a, ord=1))
