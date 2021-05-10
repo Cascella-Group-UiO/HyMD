c583211615b9f0df8fc32c4e682abc006413db7f
Samiran Sen <samiran.sen@kjemi.uio.no>
2021-05-10 13:10:05 +0200

    Add new codes to utils/

---
+++ utils/compute_density_profile.py
@@ -0,0 +1,124 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pathlib
+import h5py
+from collections.abc import Iterable
+import argparse
+
+
+def h5md_density_profile(file_path, time_steps=(0, -1), species="all",
+                         dimension="z", bins=50):
+    try:
+        in_file = h5py.File(file_path, 'r')
+        time = in_file["particles"]["all"]["position"]["time"][...]
+        positions = in_file["particles"]["all"]["position"]["value"][...]
+        species_index = in_file["parameters"]["vmd_structure"]["indexOfSpecies"][...]
+        species_names = in_file["parameters"]["vmd_structure"]["name"][...]
+        types = in_file["particles"]["all"]["species"][...]
+        in_file.close()
+    except IsADirectoryError:
+        raise
+    except FileNotFoundError:
+        raise
+    except OSError:
+        raise
+    
+    if species == "all":
+        species = tuple([s.decode("UTF-8") for s in species_names])
+    else:
+        def check_species_present(s, species_list):
+            species_list_ = [s_.decode("UTF-8") for s_ in species_list]
+            if s not in species_list_:
+                raise ValueError(
+                    f"No {species} type particles found in the system. "
+                    f"Only species present are: {species_list_}."
+                )
+            
+        if isinstance(species, Iterable):
+            for s in species:
+                check_species_present(s, species_names)
+    
+    n_steps = time.size
+    time_inds = np.arange(n_steps)
+    if isinstance(time_steps, int):
+        time_steps = (time_steps, time_steps)
+    try:
+        time_steps_ = []
+        for t in time_steps:
+            time_steps_.append(time_inds[t])
+        time_steps = tuple(time_steps_)
+    except (TypeError, IndexError) as e:
+        raise TypeError(
+            f"Could not interpret time_steps {time_steps} as a list of "
+            f"integers."
+        ) from e
+    
+
+    positions_slice = positions[time_steps[0]:time_steps[1], ...]
+    n_steps_average = positions_slice.shape[0]
+    dim = 0 if dimension == "x" else (1 if dimension == "y" else 2)
+    max_pos = np.max(positions_slice[..., dim])
+    min_pos = np.min(positions_slice[..., dim])
+    
+    for s in species:
+        t = species_index[np.where(species_names == np.string_(s))][0]
+        ind_t = np.where(types == t)
+        hist, bin_edges = np.histogram(
+            positions_slice[:, ind_t, dim], bins=bins, 
+            range=(min_pos, max_pos),
+        )
+        hist = hist.astype(np.float64) / float(n_steps_average)
+        edge_width = np.mean(np.diff(bin_edges))
+        bin_edges_ = bin_edges[:-1] + 0.5 * edge_width
+        ax.plot(bin_edges_, hist, label=str(s)+' '+str(time_steps))
+    
+    ax.set_xlabel(f"{dimension} position / nm", fontsize=13)
+    ax.set_ylabel("average number density / 1/nm³", fontsize=13)
+    plt.legend()
+
+description = """Plot density profiles from H5MD files
+
+Accumulates the densities from step `time_steps[0]` to `time_steps[1]` and 
+averages the resulting density profiles. The `species` argument may specify
+one or more species types to calculate the profile for. By default, all 
+particle types found in the system are used (corresponds to the default
+argument 'all'). The profile is computed across the `dimension` axis.
+
+Parameters
+----------
+file_path : str or pathlib.Path
+    File path to H5MD file.
+time_steps : int or tuple of int, optional
+    Start and end points for averaging the density. If one number is provided,
+    a single frame is considered for the profile and no averaging is done.
+species : str or tuple of str, optional
+    String or list of strings of particle species to plot the density for.
+    By default, all particle types found are treated.
+dimension : {'x', 'y', 'z'}
+    Dimension to compute the density profile across.
+bins : int, optional
+    Number of bins used in the density profile histogram.
+"""
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('traj_file', type=str, help='trajectory file with path. Eg: home/sim.h5')
+parser.add_argument('--start_frames', type=int, nargs="+",
+        help='First frames to be considered. Eg: -50 means range starts from 50th fram from last')
+parser.add_argument('--end_frames', type=int, nargs="+",
+        help='Last frames to be considered. Eg: -1 means range ends at the last frame')
+parser.add_argument('--dimension', type=str, default='z', help='Axis along which density profile is computed.'\
+        ' Eg: x [/y/z]')
+parser.add_argument('--species', type=str, default='all', help='species whose density profile is computed.'\
+        ' Eg: all [/W/N]')
+args = parser.parse_args()
+home_directory = pathlib.Path.home()
+file_path = args.traj_file
+
+fig, ax = plt.subplots(1, 1)
+fig.set_figwidth(8)
+fig.set_figheight(5)
+for i in range(len(args.start_frames)):
+    _ = h5md_density_profile(
+            file_path, species=args.species, time_steps=(args.start_frames[i], args.end_frames[i]), dimension=args.dimension, bins=60,
+    )
+
+plt.show()
---
+++ utils/h5md2input.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+import h5py
+
+
+def h5md_to_input(h5md_file, old_input, frame, out_path=None, overwrite=False):
+    if out_path is None:
+        out_path = os.path.join(os.path.abspath(os.path.dirname(old_input)),
+                                os.path.split(old_input)[-1] + '_new')
+    if os.path.exists(out_path) and not overwrite:
+        error_str = (f'The specified output file {out_path} already exists. '
+                     f'use overwrite=True ("-f" flag) to overwrite.')
+        raise FileExistsError(error_str)
+
+    f_in = h5py.File(old_input, 'r')
+    f_out = h5py.File(out_path, 'w')
+    new_values = h5py.File(h5md_file, 'r')
+
+    for k in f_in.keys():
+        f_in.copy(k, f_out)
+
+    in_steps = new_values['particles/all/position/step']
+    in_time = new_values['particles/all/position/time']
+
+    n_frames = len(in_steps)
+    if frame >= n_frames:
+        raise ValueError(
+            f'File {h5md_file} only contains {n_frames} frames, cannot '
+            f'extract frame #{frame}'
+        )
+    if frame < 0:
+        if n_frames + frame < 0:
+            raise ValueError(
+                f'File {h5md_file} only contains {n_frames} frames, cannot '
+                f'extract frame #{-frame} from the end'
+            )
+        frame_ = n_frames + frame
+        print(
+            f'Extracting frame #{frame_} (simulation step {in_steps[frame]} '
+            f'at time {in_time[frame]} ps)'
+        )
+
+    new_positions = new_values['particles/all/position/value'][frame, :, :]
+    f_out['coordinates'][:, :] = new_positions
+    if 'particles/all/velocity' in new_values:
+        new_velocities = new_values['particles/all/velocity/value'][frame, :, :]
+        f_out['velocities'][:, :] = new_velocities
+
+    f_in.close()
+    f_out.close()
+    new_values.close()
+
+
+if __name__ == '__main__':
+    description = 'Convert .h5md files to the hymd input format'
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('h5md_file', type=str, help='input .H5MD file name')
+    parser.add_argument('old_input_file', type=str,
+                        help='previous .H5 file name')
+    parser.add_argument('--out', type=str, default=None, dest='out_path',
+                        metavar='file name', help='output hymd HDF5 file name')
+    parser.add_argument('--frame', type=int, default=-1,
+                        help='the frame number to extract')
+    parser.add_argument('-f', action='store_true', default=False, dest='force',
+                        help='overwrite existing output file')
+    args = parser.parse_args()
+
+    h5md_to_input(args.h5md_file, args.old_input_file, args.frame,
+                  overwrite=args.force, out_path=args.out_path)
+
