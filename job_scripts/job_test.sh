c75fd55f97b4a57dfd6c3464a84ffef414e9db01
Samiran Sen <samiransen23@login-2.betzy.sigma2.no>
2021-06-15 15:34:30 +0200

    Modify job_test.sh

--- job_scripts/job_test.sh
+++ job_scripts/job_test.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
-#SBATCH --job-name=duibhai_test
+#SBATCH --job-name=test_run
 #SBATCH --account=nn4654k
 #SBATCH --time=0-1:0:0
-#SBATCH --ntasks=16
-#SBATCH --mem-per-cpu=2G
+#SBATCH --ntasks=4
 #SBATCH --qos=devel
+# ^For Betzy
 
 set -o errexit # exit on errors
 module load h5py/2.10.0-foss-2020a-Python-3.8.2
 module load pfft-python/0.1.21-foss-2020a-Python-3.8.2
 set -x
 
+export MPI_NUM_RANKS=32
 ERR_LOG="srun-${SLURM_JOB_ID}.err"
-LOG_FILE="log.txt"
-export OMP_NUM_THREADS=1
+
 export SCRATCH="/cluster/work/users/samiransen23/${SLURM_JOB_ID}"
+export HYMD_PATH=${PWD}
+
 mkdir ${SCRATCH}
 cd ${SCRATCH}
-cp /cluster/home/samiransen23/hymdlatest/configABold.toml $SCRATCH/config.toml
-cp /cluster/home/samiransen23/hymdlatest/ABinZmedwithmolecules.h5 $SCRATCH/input.h5
-mkdir hymd/
-cp /cluster/home/samiransen23/HyMD-2021/hymd/* hymd/
+
+cp ${HYMD_PATH}/$1 ${SCRATCH}/config.toml
+cp ${HYMD_PATH}/$2 ${SCRATCH}/input.h5
+mkdir hymd
+cp -r ${HYMD_PATH}/hymd/* hymd/.
+
 
 date
-srun -K --error=${ERR_LOG} -n ${SLURM_NTASKS} --mpi=pmi2  python3 hymd/main.py config.toml input.h5 --logfile=${LOG_FILE}  --seed 1 --verbose 2
+#srun -K --error=${ERR_LOG} -n ${SLURM_NTASKS} --mpi=pmi2  python3 hymd/main.py config.toml input.h5 --logfile=${LOG_FILE}  --seed 1 --verbose 2
 
-mkdir ${SLURM_SUBMIT_DIR}/out
-cp -r ${SCRATCH}/* ${SLURM_SUBMIT_DIR}/out/
+
+srun --exclusive --ntasks ${MPI_NUM_RANKS}            \
+     python3 hymd/main.py config.toml input.h5        \
+     --logfile=log.txt --verbose 2 --seed 5           \
+     --velocity-out                                         
+     #--destdir ${DEST}                               \
+     #--double-precision
+
+wait
+mkdir ${SLURM_SUBMIT_DIR}/out_test_${SLURM_JOB_ID}
+cp -r ${SCRATCH}/* ${SLURM_SUBMIT_DIR}/out__test_${SLURM_JOB_ID}
+cp ${SCRATCH}/{config.toml,input.h5} ${SLURM_SUBMIT_DIR}/out__test_${SLURM_JOB_ID}/. 
 
