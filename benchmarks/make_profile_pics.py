f96b8bc720f00ea1faa055e7aa9117d7bf1a1e5d
Manuel Carrer <manuelca@kjemi.uio.no>
2021-06-22 11:44:56 +0200

    Add routines

--- benchmarks/make_profile_pics.py
+++
@@ -1,7 +0,0 @@
-import glob
-import os
-fns=glob.glob('*/*.prof')
-
-for fn in fns:
-    fn_out='%s.png'%(fn.split('.')[0])
-    os.system('python gprof2dot.py -f pstats %s | dot -Tpng -o %s'%(fn,fn_out))
