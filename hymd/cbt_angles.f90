ce685f68a536ca589981e76944c8ff317dd6ce64
Manuel Carrer <manuelca@kjemi.uio.no>
2021-07-28 18:02:02 +0200

    Extend fortran routines

--- hymd/cbt_angles.f90
+++ hymd/cbt_angles.f90
@@ -1,4 +1,4 @@
-subroutine cbt_angle(a, b, c, box, theta)
+subroutine cbt_angle(a, b, c, box, theta, angle_force)
     ! a, b, c are directly the vectors
 ! ==============================================================================
 ! compute_angle_forces() speedup attempt.
@@ -8,36 +8,45 @@ subroutine cbt_angle(a, b, c, box, theta)
 ! Import:
 !   from compute_angle_forces import caf as compute_angle_forces__fortran
 ! ==============================================================================
-    implicit none
+  use dipole_reconstruction
+  implicit none
 
-    real(8), dimension(3),   intent(in)     :: a
-    real(8), dimension(3),   intent(in)     :: b
-    real(8), dimension(3),   intent(in)     :: c
-    real(8), dimension(3),   intent(in)     :: box
-    real(8),                intent(out)     :: theta
+  real(8), dimension(3), intent(in)  :: a
+  real(8), dimension(3), intent(in)  :: b
+  real(8), dimension(3), intent(in)  :: c
+  real(8), dimension(3), intent(in)  :: box
+  real(8),               intent(out) :: phi
+  real(8), dimension(3), intent(out) :: fa 
+  real(8), dimension(3), intent(out) :: fc
+   
 
-    real(8) :: xra, xrc
-    real(8) :: cosphi, cosphi2
-    real(8), dimension(3) :: ra, rc, ea, ec
+  real(8) :: xra, xrc
+  real(8) :: cosphi, sinphi, cosphi2
+  real(8), dimension(3) :: ra, rc, ea, ec
 
-   ! Don't loop, just calculate the angle from the the triplet given in the dihedral subroutine
-    ra = a - b
-    ra = ra - box * nint(ra / box)
+ ! Don't loop, just calculate the angle from the the triplet given in the dihedral subroutine
+  ra = a - b
+  ra = ra - box * nint(ra / box)
 
-    rc = c - b
-    rc = rc - box * nint(rc / box)
+  rc = c - b
+  rc = rc - box * nint(rc / box)
 
-    xra = 1.0d0 / sqrt(dot_product(ra, ra))
-    xrc = 1.0d0 / sqrt(dot_product(rc, rc))
+  norm_a = norm2(ra)
+  norm_c = norm2(rc)
 
-    ea = ra * xra
-    ec = rc * xrc
+  ea = ra / norm_a
+  ec = rc / norm_c
 
-    cosphi = dot_product(ea, ec)
-    cosphi2 = cosphi * cosphi
+  cosphi = dot_product(ea, ec)
+  cosphi2 = cosphi * cosphi
 
-    if (cosphi2 < 1.0) then
-        theta = acos(cosphi)
-        ! Add dipole reconstruction routine call
+  if (cosphi2 < 1.0) then
+      phi = acos(cosphi)
+      sinphi = sin(theta)
+
+      fa = (cosphi * ea - ec) / (norm_a * sinphi)
+      fc = (cosphi * ec - ea) / (norm_c * sinphi)
+      ! Add dipole reconstruction routine call
+      ! call reconstruct(ra, rc, ea, ec, norm_a, norm_c, theta, cosphi, sinphi, b, box, dipole(2 * ind -1: 2 * ind, :), trans_matrices(3 * ind - 2: 3 * ind, :, :))
     end if
 end subroutine
