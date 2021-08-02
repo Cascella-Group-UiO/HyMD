06f4a5df0901806dc514a7ad3bfa74a15e9c1db9
Manuel Carrer <manuelca@kjemi.uio.no>
2021-08-02 18:05:47 +0200

    Fix varible declaration and move to single module call

--- hymd/compute_dihedral_forces.f90
+++ hymd/compute_dihedral_forces.f90
@@ -1,29 +1,27 @@
-subroutine cdf(force, r, box, a, b, c, d, coeff, phase, energy)
-  use cbt_angle
+subroutine cdf(force, r, dipoles, trans_matrix, box, a, b, c, d, coeff, dtype, dipole_flag, energy)
+  use dipole_reconstruction
   implicit none
 
   real(4), dimension(:,:), intent(in out) :: force
   real(4), dimension(:,:), intent(in) :: r
+  real(8), dimension(:,:,:), intent(in out) :: dipoles
+  real(8), dimension(:,:,:,:), intent(in out) :: trans_matrix
   real(8), dimension(:), intent(in) :: box
   integer, dimension(:), intent(in) :: a
   integer, dimension(:), intent(in) :: b
   integer, dimension(:), intent(in) :: c
   integer, dimension(:), intent(in) :: d
-  ! integer, dimension(:), intent(in) :: type 
-  real(8), dimension(:,:), intent(in) :: coeff_v 
-  ! real(8), dimension(:,:), intent(in) :: coeff_k 
-  ! real(8), dimension(:,:), intent(in) :: coeff_g 
-  real(8), dimension(:,:), intent(in) :: phase_v 
-  ! real(8), dimension(:,:), intent(in) :: phase_k
-  ! real(8), dimension(:,:), intent(in) :: phase_g
-
+  real(8), dimension(:,:,:), intent(in) :: coeff 
+  integer, dimension(:), intent(in) :: dtype
+  integer, intent(in) :: dipole_flag
   real(8), intent(out) :: energy
    
   integer :: ind, aa, bb, cc, dd, i
-  real(8), dimension(3) :: f, g, h, v, w, sc, force_on_a, force_on_d
-  real(8), dimension(5) :: coeff_, phase_
+  real(8), dimension(3) :: f, g, h, v, w, sc, fa, fb, fc, fd
+  real(8), dimension(5) :: c_v, c_k, c_g, d_v, d_k, d_g
+  real(8) :: energy_cbt
   real(8) :: g_norm, vv, ww, fg, hg
-  real(8) ::df_dih, df_ang, cos_phi, sin_phi, phi
+  real(8) :: df_dih, df_ang, cos_phi, sin_phi, phi
 
   energy = 0.d0
   force = 0.d0
@@ -56,8 +54,8 @@ subroutine cdf(force, r, box, a, b, c, d, coeff, phase, energy)
     fg = dot_product(f, g)
     hg = dot_product(h, g)
 
-    c_v = coeff_v(ind, :)
-    d_v = phase_v(ind, :)
+    c_v = coeff(ind, 1, :)
+    d_v = coeff(ind, 2, :)
 
     df_dih = 0.d0
 
@@ -67,54 +65,51 @@ subroutine cdf(force, r, box, a, b, c, d, coeff, phase, energy)
     end do
 
     if (type == 1) then
-      c_k = coeff_k(ind, :)
-      d_k = phase_k(ind, :)
-      c_g = coeff_g(ind, :)
-      d_g = phase_g(ind, :)
+      c_k = coeff(ind, 3, :)
+      d_k = phase(ind, 4, :)
+      c_g = coeff(ind, 5, :)
+      d_g = phase(ind, 6, :)
+
       ! V = V_prop + k * (gamma - gamma_0)**2      
-      
-      ! Levitt-Warshel
-      ! gamma_0 = 106 - 13 * cos(phi - 45)
-      ! gamma_0 = 1.85d0 - 0.227d0 * cos(phi - 0.785d0)
-      ! Not used in the end?
-      ! We use another Fourier expansion?
+      call dipole_reconstruction( &
+        f, r(bb, :), -g, box, c_k, d_k, c_g, d_g, phi, dipole_flag, &
+        energy_cbt, df_cbt, fa, fc, dipole, trans_matrix)
 
-      k = 0.d0
-      gamma_0 = 0.d0
-      dk = 0.d0
-      dg = 0.d0
+      energy = energy + energy_cbt
+      df_dih = df_dih + df_cbt
 
-      do i = 0, 4 
-        k = k + c_k(i + 1) * (1.d0 + cos(i * phi + d_k(i + 1)))
-        g_0 = g_0 + c_g(i + 1) * (1.d0 + cos(i * phi + d_g(i + 1)))
-
-        dk = dk + i * c_k(i + 1) * sin(i * phi + d_k(i + 1))
-        dg = dg + i * c_g(i + 1) * sin(i * phi + d_g(i + 1))
-      end do
-
-      call cbt_angle(r(aa,:), r(bb,:), r(cc,:), box, gamm, fa, fc)
-      df_ang = -k * (gamm - gamma_0) 
-
-      var_sq = (gamm - gamma_0) * (gamm - gamma_0)
-      energy = energy + 0.5d0 * k * var_sq      
-      df_dih = df_dih + 0.5d0 * dk * var_sq + df_ang * dg
-
-      fa = df_ang * fa
-      fc = df_ang * fc
-
+      ! Angle forces
       force(aa, :) = force(aa, :) + fa
       force(bb, :) = force(bb, :) - fa - fc
       force(cc, :) = force(cc, :) + fc
+
+      if (ind == size(a)) then
+        ! calculate last angle
+        call dipole_reconstruction( &
+          g, r(cc, :), h, box, c_k, d_k, c_g, d_g, phi, dipole_flag, &
+          energy_cbt, df_cbt, fb, fd, dipole, trans_matrix)
+
+        energy = energy + energy_cbt
+
+        ! Angle forces
+        force(bb, :) = force(bb, :) + fb
+        force(cc, :) = force(cc, :) - fb - fd
+        force(dd, :) = force(aa, :) + fd
+
+      end if
     end if
- 
+
+    ! Dihedral forces
     sc = v * fg / (vv * g_norm) - w * hg / (ww * g_norm)
+
     fa = df_dih * g_norm * v / vv
     fd = df_dih * g_norm * w / ww
+    fb = df_dih * sc + fa
+    fc = df_dih * sc + fd
     
-    ! Dihedrals
     force(aa, :) = force(aa, :) - fa
-    force(bb, :) = force(bb, :) + df_dih * sc + fa
-    force(cc, :) = force(cc, :) - df_dih * sc - fd
+    force(bb, :) = force(bb, :) + fb
+    force(cc, :) = force(cc, :) - fc
     force(dd, :) = force(dd, :) + fd
   end do
 end subroutine cdf
