fa0899c7dba75c16c2ad008c6bbd0525df1b0e34
Manuel Carrer <manuelca@kjemi.uio.no>
2021-08-16 13:59:30 +0200

    Fix various typos

--- hymd/compute_dihedral_forces.f90
+++ hymd/compute_dihedral_forces.f90
@@ -4,8 +4,8 @@ subroutine cdf(force, r, dipoles, trans_matrix, box, a, b, c, d, coeff, dtype, b
 
   real(4), dimension(:,:), intent(in out) :: force
   real(4), dimension(:,:), intent(in) :: r
-  real(8), dimension(:,:,:), intent(in out) :: dipoles
-  real(8), dimension(:,:,:,:), intent(in out) :: trans_matrix
+  real(4), dimension(:,:,:), intent(in out) :: dipoles
+  real(4), dimension(:,:,:,:), intent(in out) :: trans_matrix
   real(8), dimension(:), intent(in) :: box
   integer, dimension(:), intent(in) :: a
   integer, dimension(:), intent(in) :: b
@@ -19,13 +19,16 @@ subroutine cdf(force, r, dipoles, trans_matrix, box, a, b, c, d, coeff, dtype, b
    
   integer :: ind, aa, bb, cc, dd, i
   real(8), dimension(3) :: f, g, h, v, w, sc, fa, fb, fc, fd
-  real(8), dimension(5) :: c_v, c_k, c_g, d_v, d_k, d_g
-  real(8) :: energy_cbt
+  real(8), dimension(5) :: c_v, c_k, d_v, d_k
+  ! real(8), dimension(5) :: c_g, d_g
+  real(8) :: energy_cbt, df_cbt
   real(8) :: g_norm, vv, ww, fg, hg
   real(8) :: df_dih, df_ang, cos_phi, sin_phi, phi
 
   energy = 0.d0
   force = 0.d0
+  dipoles = 0.d0
+  trans_matrix = 0.d0
 
   do ind = 1, size(a)
     aa = a(ind) + 1
@@ -67,43 +70,43 @@ subroutine cdf(force, r, dipoles, trans_matrix, box, a, b, c, d, coeff, dtype, b
 
     ! coefficients with different sizes might be problematic
     do i = 0, size(c_v) - 1
-      energy = energy + c_v(i + 1) * (1.d0 + cos(i * phi + d_v(i + 1)))
-      df_dih = df_dih + i * c_v(i + 1) * sin(i * phi + d_v(i + 1))
+      energy = energy + c_v(i + 1) * (1.d0 + cos(i * phi - d_v(i + 1)))
+      df_dih = df_dih + i * c_v(i + 1) * sin(i * phi - d_v(i + 1))
     end do
 
     if (dtype(ind) == 1) then
-      c_k = coeff(ind, 3, :)
-      d_k = phase(ind, 4, :)
-      c_g = coeff(ind, 5, :)
-      d_g = phase(ind, 6, :)
-
+      ! CBT potential
       ! V = V_prop + k * (gamma - gamma_0)**2      
-      call dipole_reconstruction( &
-        f, r(bb, :), -g, box, c_k, d_k, c_g, d_g, phi, dipole_flag, &
-        energy_cbt, df_cbt, fa, fc, dipole, trans_matrix)
+
+      c_k = coeff(ind, 3, :)
+      d_k = coeff(ind, 4, :)
+      ! c_g = coeff(ind, 5, :)
+      ! d_g = phase(ind, 6, :)
+
+      call reconstruct( &
+        f, r(bb, :), -g, box, c_k, d_k, phi, dipole_flag, &
+        energy_cbt, df_cbt, fa, fb, fc, dipoles(ind, 1:2, :), trans_matrix(ind, 1:3, :, :))
 
       energy = energy + energy_cbt
       df_dih = df_dih + df_cbt
 
       ! Angle forces
       force(aa, :) = force(aa, :) + fa
-      force(bb, :) = force(bb, :) - fa - fc
+      force(bb, :) = force(bb, :) + fb
       force(cc, :) = force(cc, :) + fc
 
       if (bb_index(ind) == 1) then
         ! calculate last angle
-        ! Use the same coefficients as the previous angle here?
-        call dipole_reconstruction( &
-          g, r(cc, :), h, box, c_k, d_k, c_g, d_g, phi, dipole_flag, &
-          energy_cbt, df_cbt, fb, fd, dipole, trans_matrix)
+        call reconstruct( &
+          g, r(cc, :), h, box, c_k, d_k, phi, dipole_flag, &
+          energy_cbt, df_cbt, fb, fc, fd, dipoles(ind, 3:4, :), trans_matrix(ind, 4:6, :, :))
 
         energy = energy + energy_cbt
 
         ! Angle forces
         force(bb, :) = force(bb, :) + fb
-        force(cc, :) = force(cc, :) - fb - fd
-        force(dd, :) = force(aa, :) + fd
-
+        force(cc, :) = force(cc, :) + fc
+        force(dd, :) = force(dd, :) + fd
       end if
     end if
 
