ea921f64d804eb61f37a7366e3eb1f00c06a208e
Morten Ledum <morten.ledum@gmail.com>
2021-12-17 13:48:19 +0100

    Update doc_pages.yml action

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -17,7 +17,7 @@ set -x
 ###################
 
 apt-get update
-apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme
+apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme sphinxcontrib.bibtex sphinxcontrib-jsmath
 
 #####################
 # DECLARE VARIABLES #
