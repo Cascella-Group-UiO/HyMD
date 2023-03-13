4f6906d70aac9bcc7056db8d85a395e3eee24017
Henrique Musseli Cezar <henrique.musseli@gmail.com>
2023-03-13 17:05:40 +0100

    Black formatting.

--- hymd/plumed.py
+++ hymd/plumed.py
@@ -16,7 +16,7 @@ class PlumedBias:
     This wraps the :code:`Plumed()` class, see
     `https://github.com/plumed/plumed2/tree/master/python`.
 
-    The :code:`PlumedBias()` object is created with the arguments from 
+    The :code:`PlumedBias()` object is created with the arguments from
     :code:`__init__` and at everystep the methods :code:`prepare()` and
     :code:`calc()` should be called.
 
@@ -31,7 +31,7 @@ class PlumedBias:
     positions : (N, D) numpy.ndarray
         Array of positions for :code:`N` particles in :code:`D` dimensions.
         Local for each MPI rank.
-        Needed because we need C-contiguous array for passing to PLUMED.       
+        Needed because we need C-contiguous array for passing to PLUMED.
     plumed_bias : (1,) numpy.ndarray
         Used as a pointer to an :code:`double` to store the bias energy.
     plumed_version : (1,) numpy.ndarray
@@ -41,6 +41,7 @@ class PlumedBias:
     ready : bool
         Stores wether the :code:`calc()` method can be called or not.
     """
+
     plumed_obj = None
     plumed_forces = None
     positions = None
@@ -51,8 +52,7 @@ class PlumedBias:
     ready = False
     verbose = None
 
-    def __init__(self, config, plumeddat, logfile, comm=MPI.COMM_WORLD,
-                 verbose=1):
+    def __init__(self, config, plumeddat, logfile, comm=MPI.COMM_WORLD, verbose=1):
         """Constructor
 
         Parameters
@@ -76,8 +76,7 @@ class PlumedBias:
             import plumed
         except ImportError:
             err_str = (
-                "You are trying to use PLUMED "
-                "but HyMD could not import py-plumed."
+                "You are trying to use PLUMED " "but HyMD could not import py-plumed."
             )
             Logger.rank0.log(logging.ERROR, err_str)
             raise ImportError(err_str)
@@ -86,16 +85,11 @@ class PlumedBias:
         self.verbose = verbose
 
         try:
-            kernel_str = (
-                "Using PLUMED_KERNEL={}".format(os.environ["PLUMED_KERNEL"])
-            )
+            kernel_str = "Using PLUMED_KERNEL={}".format(os.environ["PLUMED_KERNEL"])
         except:
             kernel_str = "The PLUMED_KERNEL environment variable is not set."
 
-        Logger.rank0.log(
-            logging.INFO, 
-            kernel_str
-        )
+        Logger.rank0.log(logging.INFO, kernel_str)
 
         try:
             self.plumed_obj = plumed.Plumed()
@@ -109,10 +103,7 @@ class PlumedBias:
 
         self.plumed_obj.cmd("getApiVersion", self.plumed_version)
         if self.plumed_version[0] <= 3:
-            err_str = (
-                "HyMD requires a PLUMED API > 3. "
-                "Use a newer PLUMED kernel."
-            )
+            err_str = "HyMD requires a PLUMED API > 3. " "Use a newer PLUMED kernel."
             Logger.rank0.log(logging.ERROR, err_str)
             raise AssertionError(err_str)
 
@@ -120,72 +111,56 @@ class PlumedBias:
         self.plumed_obj.cmd("setMPIComm", comm)
 
         Logger.rank0.log(
-            logging.INFO, 
-            f"Attempting to read PLUMED input from {plumeddat}"
+            logging.INFO, f"Attempting to read PLUMED input from {plumeddat}"
         )
 
         self.plumed_obj.cmd("setPlumedDat", plumeddat)
         self.plumed_obj.cmd("setLogFile", logfile)
-        self.plumed_obj.cmd("setTimestep", 
-            config.respa_inner * config.time_step
-        )
+        self.plumed_obj.cmd("setTimestep", config.respa_inner * config.time_step)
         self.plumed_obj.cmd("setNatoms", config.n_particles)
-        self.plumed_obj.cmd("setKbT", 
-            config.gas_constant * config.target_temperature
-        )
+        self.plumed_obj.cmd("setKbT", config.gas_constant * config.target_temperature)
         self.plumed_obj.cmd("setNoVirial")
         self.plumed_obj.cmd("init")
 
         Logger.rank0.log(
-            logging.INFO, 
-            f"Successfully read PLUMED input. PLUMED output file is {logfile}"
+            logging.INFO,
+            f"Successfully read PLUMED input. PLUMED output file is {logfile}",
         )
 
     def finalize(self):
-        """Finalize object
-        """
+        """Finalize object"""
         self.plumed_obj.finalize()
 
-
     def __enter__(self):
-        """Allow usage in 'with' context
-        """
+        """Allow usage in 'with' context"""
         return self
 
-
     def __exit__(self, exc_type, exc_value, traceback):
-        """Finalize on exit of context
-        """
+        """Finalize on exit of context"""
         self.plumed_obj.__exit__(exc_type, exc_value, traceback)
-
 
     @property
     def api_version(self):
-        """Returns the API version got from the PLUMED kernel.
-        """
+        """Returns the API version got from the PLUMED kernel."""
         return self.plumed_version[0]
-
 
     def prepare(self, step, forces, positions, indices, config, charges):
         """Set the pointers to positions and forces, and returns
         wether the potential energy is being requested by PLUMED or not.
         """
         if self.verbose > 1:
-            Logger.rank0.log(
-                logging.INFO, 
-                f"Setting PLUMED pointers for step {step}"
-            )
+            Logger.rank0.log(logging.INFO, f"Setting PLUMED pointers for step {step}")
         self.plumed_forces = forces.astype(np.double)
         self.charges = charges.astype(np.double)
-        self.positions = positions.ravel() # get C-contiguous array
+        self.positions = positions.ravel()  # get C-contiguous array
 
         needs_energy = np.zeros(1, np.intc)
         # plumed_virial = np.zeros((3,3), dtype=np.double)
         masses = np.full_like(indices, config.mass, dtype=np.double)
         box = np.diag(config.box_size)
 
-        self.plumed_obj.cmd("setAtomsNlocal", indices.shape[0]);
-        self.plumed_obj.cmd("setAtomsGatindex", indices);
+        self.plumed_obj.cmd("setAtomsNlocal", indices.shape[0])
+        self.plumed_obj.cmd("setAtomsGatindex", indices)
         self.plumed_obj.cmd("setStep", step)
         self.plumed_obj.cmd("setForces", self.plumed_forces)
         self.plumed_obj.cmd("setPositions", self.positions)
@@ -201,10 +176,9 @@ class PlumedBias:
         self.ready = True
         return True if needs_energy[0] != 0 else False
 
-
     def calc(self, forces, poteng):
-        """Passes the energy (which can be set to any value in case 
-        :code:`prepare()` returns PLUMED doesn't need it) to get 
+        """Passes the energy (which can be set to any value in case
+        :code:`prepare()` returns PLUMED doesn't need it) to get
         the forces and energy from the bias.
         """
         if not self.ready:
@@ -213,13 +187,10 @@ class PlumedBias:
                 "without first calling prepare method."
             )
             Logger.rank0.log(logging.ERROR, err_str)
-            raise RuntimeError(err_str)      
+            raise RuntimeError(err_str)
 
         if self.verbose > 1:
-            Logger.rank0.log(
-                logging.INFO, 
-                "Calculating PLUMED forces"
-            )
+            Logger.rank0.log(logging.INFO, "Calculating PLUMED forces")
 
         # set the energy and calc
         self.plumed_obj.cmd("setEnergy", poteng)
@@ -234,17 +205,13 @@ class PlumedBias:
                 "and your input should be checked. "
             )
             Logger.rank0.log(logging.ERROR, err_str)
-            raise RuntimeError(err_str)                  
+            raise RuntimeError(err_str)
 
         # subtract forces to get only the bias' extra force
         self.plumed_forces -= forces
 
         if self.verbose > 1:
-            Logger.rank0.log(
-                logging.INFO, 
-                "Done calculating PLUMED forces"
-            )
+            Logger.rank0.log(logging.INFO, "Done calculating PLUMED forces")
 
         self.ready = False
         return self.plumed_forces, self.plumed_bias[0]
-
